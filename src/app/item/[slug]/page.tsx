577935b3de0b27405a8d9203dfa80815c663fd8a
Mahmoud Zakaria <manuscrit974@gmail.com>
2025-09-21 15:23:22 +0200

    progress backend

--- src/app/item/[slug]/page.tsx
+++ src/app/item/[slug]/page.tsx
@@ -2,11 +2,13 @@ import Product from "@/components/ProductCard/Product";
 import React from "react";
 
 export default async function page() {
-  const data = await fetch('http://localhost:3000/api/users', {
+  const data = await fetch('http://localhost:3000/api/product', {
     cache: 'no-store'
   });
   
   const posts = await data.json();
+  console.log(posts);
+  
 
   
   return (
